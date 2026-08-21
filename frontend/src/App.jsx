6a3957f4579f79ec96004a9ce98a1ac6d505d31e
AxodusYT <vishalsharma113717@gmail.com>
2026-08-21 14:48:54 +0530

    made color pallet in index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>gst filing website</div>
+     <div className="min-h-screen bg-background flex items-center justify-center">
+      <h1 className="text-3xl font-semibold text-on-surface">
+        Kartsho GST
+      </h1>
+    </div>
   )
 }
 
